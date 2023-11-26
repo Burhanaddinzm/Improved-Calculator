9f5197cdf5745aed27081a77a45a72079a8b2e4f
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-11-27 00:10:07 +0400

    Improved Console Calculator

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,103 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Calculator calculator = new Calculator();
+        calculator.RunApp();
+    }
+}
+
+public class Calculator
+{
+    public void RunApp()
+    {
+        Menu();
+        string request = Console.ReadLine();
+        while (request != "0") 
+        {
+            switch (request)
+            {
+                case "1":
+                    Calculate();
+                    break;
+            }
+            Menu();
+            request = Console.ReadLine();
+        }
+    }
+    void Menu()
+    {
+        Console.WriteLine("1.Calculate");
+        Console.WriteLine("0.Close");
+    }
+
+    void Calculate()
+    {
+        Console.WriteLine("Input your operation:");
+        string input = Console.ReadLine();
+
+        int operatorIndex = input.IndexOf('+');
+        if (operatorIndex == -1)
+        {
+            operatorIndex = input.IndexOf('-');
+            if (operatorIndex == -1)
+            {
+                operatorIndex = input.IndexOf('*');
+                if (operatorIndex == -1)
+                {
+                    operatorIndex = input.IndexOf('/');
+                }
+            }
+        }
+
+        if (operatorIndex != -1)
+        {
+            string num1String = input.Substring(0, operatorIndex);
+            string num2String = input.Substring(operatorIndex + 1);
+
+            if (int.TryParse(num1String, out int num1) && int.TryParse(num2String, out int num2))
+            {
+                char op = input[operatorIndex];
+
+                int result = 0;
+                switch (op)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Division by zero.");
+                            return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Error: Unsupported operator.");
+                        return;
+                }
+
+                Console.WriteLine($"{num1} {op} {num2} = {result}");
+            }
+            else
+            {
+                Console.WriteLine("Error: Unable to parse numbers.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No operator found in the input.");
+        }
+    }
+
+}
